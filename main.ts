53f14989f3adfa8b93ca0dd094264580bcd5f868
Patihan Prasongphol <105841902+NinePTH@users.noreply.github.com>
2023-10-17 18:36:35 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,6 @@ input.onButtonPressed(Button.A, function () {
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 basic.forever(function () {
-    huskylens.request()
-    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Yes)
-        iBIT.MotorStop()
-    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showIcon(IconNames.Happy)
-        iBIT.MotorStop()
-    } else {
-        iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 10)
-        iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 10)
-    }
+    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 100)
+    iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 75)
 })
