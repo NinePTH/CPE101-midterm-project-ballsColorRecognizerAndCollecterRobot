ffd41f1c46a754de1bda45843d41f4dde8ac87d8
Patihan Prasongphol <105841902+NinePTH@users.noreply.github.com>
2023-10-18 19:09:50 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,34 @@
+function white () {
+    iBIT.Servo(ibitServo.SV1, 110)
+    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 44)
+    iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 50)
+}
 input.onButtonPressed(Button.A, function () {
-    iBIT.MotorStop()
+    Red()
 })
-huskylens.initI2c()
-huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
+function close () {
+    iBIT.Servo(ibitServo.SV1, 65)
+}
+input.onButtonPressed(Button.AB, function () {
+    Blue()
+})
+input.onButtonPressed(Button.B, function () {
+    white()
+})
+function forward () {
+	
+}
+function Blue () {
+    iBIT.Servo(ibitServo.SV1, 180)
+    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 44)
+    iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 50)
+}
+function Red () {
+    iBIT.Servo(ibitServo.SV1, 35)
+    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 44)
+    iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 50)
+}
+basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
-    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 100)
-    iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 75)
+	
 })
