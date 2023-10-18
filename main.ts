2cf188865cace1d392938a30e1cfd14823aab365
Patihan Prasongphol <105841902+NinePTH@users.noreply.github.com>
2023-10-18 22:48:45 +0700

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,37 +1,70 @@
+function check () {
+    huskylens.request()
+    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        iBIT.MotorStop()
+        basic.showIcon(IconNames.Yes)
+        Red()
+    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        iBIT.MotorStop()
+        basic.showIcon(IconNames.Happy)
+        white()
+    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        iBIT.MotorStop()
+        basic.showIcon(IconNames.Duck)
+        Blue()
+    }
+}
 function forwardToCollect () {
-    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 33)
-    iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 38)
+    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 25)
+    iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 50)
 }
 function Collect () {
     basic.pause(200)
-    while (second < 5) {
+    while (second < 3) {
         second += 1
         forwardToCollect()
         basic.showNumber(second)
     }
     second = 0
     iBIT.MotorStop()
-    basic.showNumber(second)
+    close()
+    basic.showLeds(`
+        . # # # .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
 }
 function white () {
     iBIT.Servo(ibitServo.SV1, 110)
     Collect()
 }
 input.onButtonPressed(Button.A, function () {
-    Normforward()
+    for (let index = 0; index < 6; index++) {
+        while (second < 2) {
+            second += 1
+            Normforward()
+            basic.showNumber(second)
+        }
+        second = 0
+        basic.pause(100)
+        iBIT.MotorStop()
+        check()
+    }
 })
 function close () {
     iBIT.Servo(ibitServo.SV1, 70)
 }
 function Normforward () {
-    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 44)
+    iBIT.setMotor(ibitMotorCH.M1, ibitMotor.Forward, 34)
     iBIT.setMotor(ibitMotorCH.M2, ibitMotor.Forward, 50)
 }
 input.onButtonPressed(Button.AB, function () {
-    iBIT.Servo(ibitServo.SV1, 35)
+	
 })
 input.onButtonPressed(Button.B, function () {
-    iBIT.Servo(ibitServo.SV1, 180)
+    iBIT.Servo(ibitServo.SV1, 70)
 })
 function Blue () {
     iBIT.Servo(ibitServo.SV1, 180)
@@ -46,20 +79,5 @@ huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_COLOR_RECOGNITION)
 basic.showIcon(IconNames.Ghost)
 basic.forever(function () {
-    huskylens.request()
-    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        iBIT.MotorStop()
-        basic.showIcon(IconNames.Yes)
-        Red()
-    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        iBIT.MotorStop()
-        basic.showIcon(IconNames.Happy)
-        white()
-    } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        iBIT.MotorStop()
-        basic.showIcon(IconNames.Duck)
-        Blue()
-    } else {
-    	
-    }
+	
 })
